985dd50719b80a980f5f22f4a49b9f5c9a3dd16c
Paul Angeles <paul.angeles.chaquire@gmail.com>
2025-11-19 05:21:34 +0100

    add why me & skills section frame

--- portfolio/src/app/core/main-content/main-content.ts
+++ portfolio/src/app/core/main-content/main-content.ts
@@ -1,8 +1,13 @@
 import { Component, AfterViewInit, ElementRef } from '@angular/core';
+import { Hero } from '../../sections/hero/hero';
+import { WhyMe } from '../../sections/why-me/why-me';
+import { Skills } from '../../sections/skills/skills';
+import { Projects } from '../../sections/projects/projects';
+import { Contact } from '../../sections/contact/contact';
 
 @Component({
   selector: 'app-main-content',
-  imports: [],
+  imports: [Hero, WhyMe, Skills, Projects, Contact],
   templateUrl: './main-content.html',
   styleUrl: './main-content.scss',
 })
@@ -13,12 +18,12 @@ export class MainContent implements AfterViewInit {
     const container = this.elementRef.nativeElement.querySelector('.main-container');
 
     container.addEventListener('wheel', (event: WheelEvent) => {
-      event.preventDefault();
+      event.preventDefault(); // Stoppt vertikales Scrollen
 
-      // Scroll-Wert verstärken
-      const scrollAmount = event.deltaY * 3; // <- 2x stärker!
+      // Scroll-Wert verstärken (statt nur deltaY)
+      const scrollAmount = event.deltaY * 3; // <- 3x stärker!
       container.scrollLeft += scrollAmount;
 
-    }, { passive: false }); // <- Das ist der wichtige Teil!
+    }, { passive: false }); // <- Wichtig für preventDefault()
   }
 }
