eeb124e6add43f0e09ad1164e0aef845e39e73db
Paul Angeles <paul.angeles.chaquire@gmail.com>
2025-11-22 01:05:55 +0100

    build why me section

--- portfolio/src/app/sections/why-me/why-me.ts
+++ portfolio/src/app/sections/why-me/why-me.ts
@@ -3,10 +3,11 @@ import { TranslateModule } from "@ngx-translate/core";
 import { SectionContent } from '../../shared/components/section-content/section-content';
 import { SectionBow } from '../../shared/components/section-bow/section-bow';
 import { Button } from '../../shared/components/button/button';
+import { Details } from '../../shared/components/details/details';
 
 @Component({
   selector: 'app-why-me',
-  imports: [TranslateModule, SectionBow, SectionContent, Button],
+  imports: [TranslateModule, SectionBow, SectionContent, Button, Details],
   templateUrl: './why-me.html',
   styleUrl: './why-me.scss',
 })
---
+++ portfolio/src/app/shared/components/details/details.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-details',
+  imports: [TranslateModule],
+  templateUrl: './details.html',
+  styleUrl: './details.scss',
+})
+export class Details {
+  @Input() icon: string = '';
+  @Input() detail: string = '';
+}
