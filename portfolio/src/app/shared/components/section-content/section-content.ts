1972f3ae8a2bebe845915df433ff6506f177ecd5
Paul Angeles <paul.angeles.chaquire@gmail.com>
2025-11-23 20:52:12 +0100

    upd skill

--- portfolio/src/app/shared/components/section-content/section-content.ts
+++ portfolio/src/app/shared/components/section-content/section-content.ts
@@ -1,6 +1,5 @@
 import { Component, Input } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
-import { Details } from '../details/details';
 
 @Component({
   selector: 'app-section-content',
