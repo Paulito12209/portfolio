9bcf52fe65547c4363ce1149e84009391af2870d
Paul Angeles <paul.angeles.chaquire@gmail.com>
2025-11-24 02:20:44 +0100

    add projects page

--- portfolio/src/app/sections/projects/projects.ts
+++ portfolio/src/app/sections/projects/projects.ts
@@ -1,14 +1,47 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { SectionContent } from '../../shared/components/section-content/section-content';
 import { SectionBow } from '../../shared/components/section-bow/section-bow';
+import { ProjectDetails } from '../../shared/components/project-details/project-details';
 
 @Component({
   selector: 'app-projects',
-  imports: [TranslateModule, SectionContent, SectionBow],
+  imports: [CommonModule, TranslateModule, SectionContent, SectionBow, ProjectDetails],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
 export class Projects {
-
+  projects = [
+    {
+      number: '1',
+      title: 'Project Join',
+      description: 'PROJECTS.JOIN_DESCRIPTION',
+      technologies: 'Angular, TypeScript, HTML, CSS, Firebase',
+      learning: 'PROJECTS.JOIN_LEARNING',
+      image: './img/Screen_Join.png',
+      liveLink: '#',
+      githubLink: '#'
+    },
+    {
+      number: '2',
+      title: 'Project Pollo Loco',
+      description: 'PROJECTS.POLLO_DESCRIPTION',
+      technologies: 'JavaScript, HTML, CSS',
+      learning: 'PROJECTS.POLLO_LEARNING',
+      image: './img/Screen_Pollo-loco.png',
+      liveLink: '#',
+      githubLink: '#'
+    },
+    {
+      number: '3',
+      title: 'Project DA Bubble',
+      description: 'PROJECTS.BUBBLE_DESCRIPTION',
+      technologies: 'JavaScript, HTML, CSS',
+      learning: 'PROJECTS.BUBBLE_LEARNING',
+      image: './img/Screen_DA-Bubble.png',
+      liveLink: '#',
+      githubLink: '#'
+    }
+  ];
 }
--- portfolio/src/app/shared/components/project-details/project-details.ts
+++ portfolio/src/app/shared/components/project-details/project-details.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+import { Button } from '../button/button';
 
 @Component({
   selector: 'app-project-details',
-  imports: [],
+  imports: [TranslateModule, Button],
   templateUrl: './project-details.html',
   styleUrl: './project-details.scss',
 })
 export class ProjectDetails {
-
+  @Input() number: string = '';
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() technologies: string = '';
+  @Input() learning: string = '';
+  @Input() image: string = '';
+  @Input() githubLink: string = '';
+  @Input() liveLink: string = '';
 }
