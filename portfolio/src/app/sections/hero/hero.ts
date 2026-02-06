3d6ebf217285f9b521da9358f222e8a1cee9be85
Paul Angeles <paul.angeles.chaquire@gmail.com>
2026-02-06 16:44:33 +0100

    fix unnecessary header import

--- portfolio/src/app/sections/hero/hero.ts
+++ portfolio/src/app/sections/hero/hero.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from "@ngx-translate/core";
-import { Header } from '../../shared/components/header/header';
 import { SectionBow } from '../../shared/components/section-bow/section-bow';
 
 @Component({
   selector: 'app-hero',
-  imports: [TranslateModule, Header, SectionBow],
+  imports: [TranslateModule, SectionBow],
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
